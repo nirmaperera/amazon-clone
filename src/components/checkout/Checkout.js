4c7e106b4e1a098e41dbfb2f25eb1e569d2b1a27
Nirma Perera <nirmaperera23@gmail.com>
2020-09-28 16:51:24 -0400

    subtotal component added

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Checkout.css';
 
+import SubTotal from './Subtotal';
+
 function Checkout() {
     return (
         <div className="checkout-container">
@@ -10,15 +12,12 @@ function Checkout() {
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                     alt=""
                 />
-
                 <div>
                     <h2 className="checkout-title">Your shopping cart</h2>
                 </div>
             </div>
             <div className="checkout-right">
-                <h2>The subtotal</h2>
-
-
+                <SubTotal />
             </div>
         </div>
 
